93d84297b220f0ddcaba96f241dea451dcc3a5d5
Adam Korynta <adam@rmanet.com>
2026-07-06 10:16:57 -0700

    fix dash vs underscore

--- regi-headless/src/main/python/regi_python/__init__.py
+++ regi-headless/src/main/python/regi_python/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import version, PackageNotFoundError
 __all__ = ["regi_session", "run_headless"]
 
 try:
-    __version__ = version("regi_python") # Use the 'name' from pyproject.toml
+    __version__ = version("regi_python")
 except PackageNotFoundError:
     # package is not installed
     __version__ = "unknown"
