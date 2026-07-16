3c8794442ea1504f05e12b9a7447b4ab43dec320
Adam Korynta <adam@rmanet.com>
2026-07-16 13:34:57 -0700

    migrate script examples from Jython to regi-python library
    remove all redundant example scripts

--- regi-headless/src/test/resources/usace/rowcps/headless/examples/InflowCalcBalanceAll.py
+++ regi-headless/src/test/resources/usace/rowcps/headless/examples/InflowCalcBalanceAll.py
@@ -1,36 +1,38 @@
-# the java Calendar class is used to create java Date objects
-from java.util import Calendar
-from java.util import TimeZone
-
-# this gets a scriptable Pool Percent object
-inflowCalc = registry.getCalculation(1.0, "Inflow")
-
-# configure the start calendar
-
-startCal = Calendar.getInstance(TimeZone.getTimeZone('US/Central'))
+from regi_python import regi_session, run_headless
 
 
-startCal.clear()
-startCal.set(Calendar.YEAR, 2015)
-startCal.set(Calendar.MONTH, 4)
+def calculate_inflow(registry):
+    # Java imports must happen after regi_session starts the JVM.
+    from java.util import Calendar, TimeZone
+
+    # this gets a ScriptableInflow instance.
+    inflow_calc = registry.getCalculation(1.0, "Inflow")
+
+    # configure the start calendar
+    start_cal = Calendar.getInstance(TimeZone.getTimeZone("US/Central"))
+    start_cal.clear()
+    start_cal.set(Calendar.YEAR, 2018)
+    start_cal.set(Calendar.MONTH, 7)
+
+    # inflow_calc contains 4 callable methods:
+    # autoAdjust
+    # balanceAll
+    # cloneInflows
+    # zeroNegatives
+
+    # Each method takes the following arguments:
+    #   officeId
+    #   locationId
+    #   startDate
+
+    # autoAdjust also takes booleans:
+    #   useLimits
+    #   freezeRain
+
+    # This autoBalances ALAT2
+    inflow_calc.balanceAll("SWF", "ALAT2", start_cal.getTime())
 
 
-# inflowCalc contains 4 callable methods:
-# autoAdjust
-# balanceAll
-# cloneInflows
-# zeroNegatives
-
-# Each method takes the followind arguments:
-#   officeId
-#   locationId
-#   startDate
-
-# autoAdjust also takes booleans:
-#	useLimits
-#	freezeRain
-
-# This autoBalances ALAT2
-inflowCalc.balanceAll("SWF", "ALAT2",  startCal.getTime())
-
-
+if __name__ == "__main__":
+    with regi_session():
+        run_headless(calculate_inflow)
