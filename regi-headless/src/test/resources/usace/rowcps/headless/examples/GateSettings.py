783e9d74aaeeb66f55c1a38a8b1ed359b4d8a64a
Adam Korynta <47677856+adamkorynta@users.noreply.github.com>
2026-08-21 11:27:34 -0700

    Feature/jpype script migration (#23)
    * migrate Java/Jython focused REGI Headless to regi-python via jPype
    
    * migrate script examples from Jython to regi-python library
    
    remove all redundant example scripts
    
    * add smoke testing now that script migration is complete

--- regi-headless/src/test/resources/usace/rowcps/headless/examples/GateSettings.py
+++ regi-headless/src/test/resources/usace/rowcps/headless/examples/GateSettings.py
@@ -1,42 +1,46 @@
-# the java Calendar class is used to create java Date objects
-from java.util import Calendar
-from java.util import TimeZone
-
-# this gets a scriptable Gate Settings object
-gateSettings = registry.getCalculation(1.0, "Gate Settings")
-
-# Time zone must be set because the Solaris time zone is UTC
-timeZone = TimeZone.getTimeZone("US/Central")
-# configure the start calendar
-startCal = Calendar.getInstance(timeZone)
-startCal.clear()
-startCal.set(Calendar.YEAR, 2015)
-startCal.set(Calendar.MONTH, 4)
-
-# configure the end calendar
-endCal = Calendar.getInstance(timeZone)
-endCal.clear()
-endCal.set(Calendar.YEAR, 2015)
-endCal.set(Calendar.MONTH, 6)
+from regi_python import regi_session, run_headless
 
 
-# gateSettings contains four callable methods
-#   void createGateSettings(String officeId, String locationStr, Date startDate, Date end) throws Exception;
-#	void createGateSettingsGroup(String officeId, String locationStr, Date startDate, Date end, String groupId) throws Exception;
-#	void createGateSettingsOutlet(String officeId, String locationStr, Date startDate, Date end, String outletId) throws Exception;
-#	void createGateSettingsOutletFromTs(String officeId, String locationStr, Date startDate, Date end, String outletId, String tsId) throws Exception;
+def calculate_gate_settings(registry):
+    # Java imports must happen after regi_session starts the JVM.
+    from java.util import Calendar, TimeZone
 
-# This is an example of a call that would create gate settings at TainterGate 1 at WTYT2  from the specified input time series.
-gateSettings.createGateSettingsOutletFromTs("SWF", "WTYT2",  startCal.getTime(), endCal.getTime(), "TG1", "WTYT2-TG1.Opening-Spillway_Gate.Const.0.0.MANUAL" )
+    # this gets a scriptable Gate Settings object
+    gate_settings = registry.getCalculation(1.0, "Gate Settings")
 
-# This is an example of a call that would create gate settings at TainterGate 1 at WTYT2 from the regi association configured input time series.
-gateSettings.createGateSettingsOutlet("SWF", "WTYT2",  startCal.getTime(), endCal.getTime(), "TG1")
+    # Time zone must be set because the Solaris time zone is UTC
+    time_zone = TimeZone.getTimeZone("US/Central")
+    # configure the start calendar
+    start_cal = Calendar.getInstance(time_zone)
+    start_cal.clear()
+    start_cal.set(Calendar.YEAR, 2015)
+    start_cal.set(Calendar.MONTH, 4)
 
-# This is an example of a call that would create gate settings at all outlets at WTYT2 which are in the TainterGateWTY group from the association configured time series.
-gateSettings.createGateSettingsGroup("SWF", "WTYT2",  startCal.getTime(), endCal.getTime(), "WTYT2-TainterGateWTY" )
+    # configure the end calendar
+    end_cal = Calendar.getInstance(time_zone)
+    end_cal.clear()
+    end_cal.set(Calendar.YEAR, 2015)
+    end_cal.set(Calendar.MONTH, 6)
 
-# This is an example of a call that would create gate settings for every outlet in a rating group at WTYT2.
-gateSettings.createGateSettings("SWF", "WTYT2",  startCal.getTime(), endCal.getTime() )
+    # gate_settings contains four callable methods
+    #   void createGateSettings(String officeId, String locationStr, Date startDate, Date end) throws Exception;
+    #   void createGateSettingsGroup(String officeId, String locationStr, Date startDate, Date end, String groupId) throws Exception;
+    #   void createGateSettingsOutlet(String officeId, String locationStr, Date startDate, Date end, String outletId) throws Exception;
+    #   void createGateSettingsOutletFromTs(String officeId, String locationStr, Date startDate, Date end, String outletId, String tsId) throws Exception;
+
+    # This is an example of a call that would create gate settings at TainterGate 1 at WTYT2 from the specified input time series.
+    gate_settings.createGateSettingsOutletFromTs("SWF", "WTYT2", start_cal.getTime(), end_cal.getTime(), "TG1", "WTYT2-TG1.Opening-Spillway_Gate.Const.0.0.MANUAL")
+
+    # This is an example of a call that would create gate settings at TainterGate 1 at WTYT2 from the regi association configured input time series.
+    gate_settings.createGateSettingsOutlet("SWF", "WTYT2", start_cal.getTime(), end_cal.getTime(), "TG1")
+
+    # This is an example of a call that would create gate settings at all outlets at WTYT2 which are in the TainterGateWTY group from the association configured time series.
+    gate_settings.createGateSettingsGroup("SWF", "WTYT2", start_cal.getTime(), end_cal.getTime(), "WTYT2-TainterGateWTY")
+
+    # This is an example of a call that would create gate settings for every outlet in a rating group at WTYT2.
+    gate_settings.createGateSettings("SWF", "WTYT2", start_cal.getTime(), end_cal.getTime())
 
 
-
+if __name__ == "__main__":
+    with regi_session():
+        run_headless(calculate_gate_settings)
